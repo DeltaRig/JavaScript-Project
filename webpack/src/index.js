c74d5802bb08ab93178b7431b263ac920595ec80
AMERICAS\Daniela_Rigoli <daniela_rigoli@dell.com>
2022-02-01 16:12:20 -0300

    Webpack and modern JS

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,4 +1,13 @@
 "use strict";
+
+var tools = require("./calculate.js");  
+ 
+var value = tools.sum(10,20);  
+ 
+console.log("Value: "+value); 
+
+
+
 var costByPerson = 50;
 
 // function constructor 
