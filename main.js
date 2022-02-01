f3065ba36d8d93474d447d8a72e0c8d6e225232b
AMERICAS\Daniela_Rigoli <daniela_rigoli@dell.com>
2022-02-01 09:25:18 -0300

    Add primitive, objects and work with functions

--- main.js
+++ main.js
@@ -7,9 +7,22 @@ function Employee(experience, skills){
 
 // creating calculateAge() method to the Prototype property 
 Employee.prototype.calculateCost = function(){  
-    console.log('The current cost is: '+(this.experience * this.skills * 300)); 
+    var mult = 300; // Number
+    var result = (this.experience * this.skills * mult);
+    console.log('The current cost to this employee is: '+ result); 
+    return result;
 } 
 console.log(Employee.prototype); 
+
+function allCost(team) {
+    var cost = 0;
+    for (var i = 0; i < team.length; i++) {
+        console.log(team[i]) 
+        cost += team[i].calculateCost(); 
+    }
+    return cost;
+}
+
 
 // creating Objects
 let Emp1= new Employee(1, 4);
@@ -18,9 +31,7 @@ let Emp2= new Employee(2, 7);
 
 let Emp3= new Employee(3, 10); 
 
-var team = [Emp1, Emp2, Emp3];
+var team = [Emp1, Emp2, Emp3]; //Array
 
-for (var i = 0; i < team.length; i++) {
-    console.log(team[i]) 
-    team[i].calculateCost(); 
-}
+var allCost = allCost(team);
+console.log("Cost with all team: " + allCost);
